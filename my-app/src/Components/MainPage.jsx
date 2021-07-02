5ae4d4d972d9829025a3f16ec0d422effe79bac3
Maria Shcherbatykh <mariashcherbatykh2001@gmail.com>
2021-07-02 23:03:12 +0400

    Changed file of styles MainPage from .css to .module.css. Changed styles items on page.

--- my-app/src/Components/MainPage.jsx
+++ my-app/src/Components/MainPage.jsx
@@ -1,10 +1,10 @@
-import './../MainPage.css';
+import cl from './MainPage.module.css';
 
 function MainPage() {
   return (
-    <div className="header">
-      <h1>QuizApp</h1>
-      <div className="buttons">
+    <div className={cl.content}>
+      <p className={cl.header}>QuizApp</p>
+      <div className={cl.buttons}>
         <input  type = "button" id = "quiz" value = "Квиз"></input>
         <input type = "button" id = "history" value = "История"></input>
       </div>
