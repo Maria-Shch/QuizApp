01f71a47cc7145ada95ed129d2ec25ffb588b0cb
Maria Shcherbatykh <mariashcherbatykh2001@gmail.com>
2021-07-02 14:15:36 +0400

    Changed component MainPage from .js to .jsx and moved to folder Components

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-import MainPage from './MainPage';
+import MainPage from './Components/MainPage.jsx';
 
 ReactDOM.render(
   <React.StrictMode>
