3ba225d31a1c26af260b873e4357abbf394396c3
Maria Shcherbatykh <mariashcherbatykh2001@gmail.com>
2021-07-06 01:43:26 +0400

    On the page with question, you can switch between questions by changing the value of the variable 'questionNumber' from 0 to 9. These values ​​mean the index of the array in which the questions are stored. Changed styles of components on QuestionPage.

--- my-app/src/Components/QuestionPage/QuestionPage.jsx
+++ my-app/src/Components/QuestionPage/QuestionPage.jsx
@@ -5,16 +5,25 @@ import { parsingOfQuestions } from './../../scripts/parsingOfQuestions.js'
 
 function QuestionPage() {
   let questions = parsingOfQuestions();
+
+  // Переменная questionNumber означает индекс вопроса в массиве questions
+  // Изменяя её значение, можно отображать тот или иной вопрос из массива
+  let questionNumber = 0;
+  
   return (
     <div className={cl.content}>
       <p className={cl.header}>
-        {questions[0].serialNumber}/{questions[0].numberOfQuestions}. {questions[0].question}
+        {questions[questionNumber].serialNumber}/{questions[questionNumber].numberOfQuestions}. {questions[questionNumber].question}
       </p>
       <div className={cl.possibleAnswers}>
-        <PossibleAnswer id={questions[0].answers[0]} value={questions[0].answers[0]} />
-        <PossibleAnswer id={questions[0].answers[1]} value={questions[0].answers[1]} />
-        <PossibleAnswer id={questions[0].answers[2]} value={questions[0].answers[2]} />
-        <PossibleAnswer id={questions[0].answers[3]} value={questions[0].answers[3]} />
+        <div className={cl.possibleAnswers_row}>
+          <PossibleAnswer id={questions[questionNumber].answers[0]} value={questions[questionNumber].answers[0]} />
+          <PossibleAnswer id={questions[questionNumber].answers[1]} value={questions[questionNumber].answers[1]} />
+        </div>
+        <div className={cl.possibleAnswers_row}>
+          <PossibleAnswer id={questions[questionNumber].answers[2]} value={questions[questionNumber].answers[2]} />
+          <PossibleAnswer id={questions[questionNumber].answers[3]} value={questions[questionNumber].answers[3]} />
+        </div>
       </div>
       <div className={cl.div_next}>
         <Button className='ButtonQuestionPage' id='next' value='Дальше' path='next' />
