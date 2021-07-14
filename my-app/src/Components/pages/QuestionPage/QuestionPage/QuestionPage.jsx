97f88a4651508292c9d4a0ef2363c4d5e3b48e05
Maria Shcherbatykh <mariashcherbatykh2001@gmail.com>
2021-07-14 18:17:35 +0400

    An intermediate working option for switching questions without reloading the page.

--- my-app/src/Components/pages/QuestionPage/QuestionPage/QuestionPage.jsx
+++ my-app/src/Components/pages/QuestionPage/QuestionPage/QuestionPage.jsx
@@ -5,51 +5,78 @@ import Button from '../../../shared/buttons/Button/Button';
 import ButtonNext from '../../../shared/buttons/ButtonNext/ButtonNext';
 import PossibleAnswers from '../PossibleAnswers/PossibleAnswers';
 import { getQuizById, parsingQuizToQuestions } from '../../../../utils/utilsFunctions';
+import React from 'react';
 
-function QuestionPage(props) {
-  const quizId = '07e6bda'; 
-  const quiz = getQuizById(quizId);
-  const questions = parsingQuizToQuestions(quiz);
+class QuestionPage extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      questionNumber: props.match.params.numberQuestion,
+      nextQuestion: Number(props.match.params.numberQuestion) + 1
+    };
+    this.next = this.next.bind(this);
+  }
+  //При достижении последнего вопроса в квизе значение кнопки меняется с 'Дальше' на 'Закончить'
+  valueOfButton(questionNumber, numberOfQuestions) {
+    if (questionNumber < numberOfQuestions) return 'Дальше';
+    else return 'Закончить';
+  }
 
-  const questionNumber = props.match.params.numberQuestion;
-  const indexArray = questionNumber - 1;
-  const nextQuestion = Number(questionNumber) + 1;
-  const numberOfQuestions = questions.length;
+  //При достижении последнего вопроса нажатие кнопки будет перенаправлять на страницу с результатом
+  checkPath(nextQuestion, numberOfQuestions) {
+    if (this.state.nextQuestion <= numberOfQuestions) return this.state.nextQuestion;
+    else return './../result';
+  }
 
-  return (
-    <div className={classNames(clIndex.content, cl.content)}>
-      <p className={classNames(clIndex.header, cl.header)}>
-        {questionNumber}/{numberOfQuestions}. {questions[indexArray].question}
-      </p>
-      <PossibleAnswers answers = {questions[indexArray].answers}/>
-      <div className={cl.div_next}>
-        <ButtonNext 
-          question={questions[indexArray]} 
-          answers={questions[indexArray].answers} 
-          className='ButtonQuestionPage' 
-          id='next' 
-          value={valueOfButton(questionNumber, numberOfQuestions)} 
-          path={checkPath(nextQuestion, numberOfQuestions)} 
-        />
+  next(path) {
+    this.setState({
+      questionNumber: this.state.nextQuestion,
+      nextQuestion: Number(this.state.questionNumber) + 2
+    });
+    window.history.pushState(null, null, path);
+  }
+
+  render() {
+    const quizId = '07e6bda';
+    const quiz = getQuizById(quizId);
+    const questions = parsingQuizToQuestions(quiz);
+
+    const indexArray = this.state.questionNumber - 1;
+    const numberOfQuestions = questions.length;
+    return (
+      <div className={classNames(clIndex.content, cl.content)}>
+        <p className={classNames(clIndex.header, cl.header)}>
+          {this.state.questionNumber}/{numberOfQuestions}. {questions[indexArray].question}
+        </p>
+        <PossibleAnswers answers={questions[indexArray].answers} />
+        <div className={cl.div_next}>
+          <button
+            onClick={() => this.next(this.state.nextQuestion)}
+            type="submit">
+            Дальше
+          </button>
+
+
+          
+          {/* <ButtonNext
+            onClick={() => this.next()}
+            question={questions[indexArray]}
+            answers={questions[indexArray].answers}
+            className='ButtonQuestionPage'
+            id='next'
+            value={valueOfButton(this.state.questionNumber, numberOfQuestions)}
+            path={checkPath(nextQuestion, numberOfQuestions)}
+          /> */}
+        </div>
+        <div className={clIndex.div_buttons}>
+          <Button className='ButtonQuestionPage' id='index' value='На главную' path='/../index' />
+          <Button className='ButtonQuestionPage' id='history' value='История' path='/../history' />
+        </div>
       </div>
-      <div className={clIndex.div_buttons}>
-        <Button className='ButtonQuestionPage' id='index' value='На главную' path='/../index' />
-        <Button className='ButtonQuestionPage' id='history' value='История' path='/../history' />
-      </div>
-    </div>
-  );
+    );
+  }
 }
 
-//При достижении последнего вопроса в квизе значение кнопки меняется с 'Дальше' на 'Закончить'
-function valueOfButton(questionNumber, numberOfQuestions) {
-  if (questionNumber < numberOfQuestions) return 'Дальше';
-  else return 'Закончить';
-}
 
-//При достижении последнего вопроса нажатие кнопки будет перенаправлять на страницу с результатом
-function checkPath(nextQuestion, numberOfQuestions) {
-  if (nextQuestion <= numberOfQuestions) return nextQuestion;
-  else return './../result';
-}
 
 export default QuestionPage;
